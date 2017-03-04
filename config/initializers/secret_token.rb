cd6bbffb5a40745b6dc6c5afeb6c41c065449234
Teruyo <yfateru@hotmail.com>
2017-03-04 13:44:37 +0000

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TicketApp::Application.config.secret_key_base = '5488114063ab3d9ae72d907391c53b754d3828c12e3de6b306c81c23b685600eed972132bdcb3153f006385dd5cab76f63322e469d64b778c236832c5c7251a5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TicketApp::Application.config.session_store :cookie_store, key: '_ticket_app_session'
